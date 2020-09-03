8103a1204e38c134c8d98653461501607945d631
Nusiba Ibrahim <ainusiba@gmail.com>
2020-09-03 07:39:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,12 +8,3 @@ function map(arr, func){
     return newArr
 }
 
-function reduce(src, fnct, start){
-    let arr = (!!start) ? start : src[0];
-    let i = (!!start) ? 0 : 1 
-
-    for (; i < src.length; i++){
-        arr = fnct(src[i], arr)
-    }
-    return arr;
-}
